cd6d3dc55d032e5ba2593a223f9e60f5a1dbda1a
Ha Nguyen <captainnemot1k60@gmail.com>
2024-06-09 01:47:09 +0700

    chore: Update MongoDB connection settings to use environment variables

--- tests/listUser.test.js
+++ tests/listUser.test.js
@@ -1,41 +1,41 @@
-// const request = require('supertest');
-// const sinon = require('sinon');
-// const app = require('../app');
-// const UserModel = require('../models/Users');
+const request = require('supertest');
+const sinon = require('sinon');
+const app = require('../app');
+const UserModel = require('../models/Users');
 
-// describe('GET /', () => {
-//     it('should return list of users', async () => {
-//         // Arrange
-//         const expectedUsers = [
-//             { _id: '1', name: 'User 1' },
-//             { _id: '2', name: 'User 2' }
-//         ];
-//         sinon.stub(UserModel, 'find').resolves(expectedUsers);
+describe('GET /', () => {
+    it('should return list of users', async () => {
+        // Arrange
+        const expectedUsers = [
+            { _id: '1', name: 'User 1' },
+            { _id: '2', name: 'User 2' }
+        ];
+        sinon.stub(UserModel, 'find').resolves(expectedUsers);
 
-//         // Act
-//         const response = await request(app).get('/');
+        // Act
+        const response = await request(app).get('/');
 
-//         // Assert
-//         expect(response.status).toBe(200);
-//         expect(response.body).toEqual(expectedUsers);
+        // Assert
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(expectedUsers);
 
-//         // Restore the stub
-//         UserModel.find.restore();
-//     });
+        // Restore the stub
+        UserModel.find.restore();
+    });
 
-//     it('should handle errors', async () => {
-//         // Arrange
-//         const errorMessage = 'Internal Server Error';
-//         sinon.stub(UserModel, 'find').rejects(new Error(errorMessage));
+    it('should handle errors', async () => {
+        // Arrange
+        const errorMessage = 'Internal Server Error';
+        sinon.stub(UserModel, 'find').rejects(new Error(errorMessage));
 
-//         // Act
-//         const response = await request(app).get('/');
+        // Act
+        const response = await request(app).get('/');
 
-//         // Assert
-//         expect(response.status).toBe(400);
-//         expect(response.body).toEqual({ error: errorMessage });
+        // Assert
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual({ error: errorMessage });
 
-//         // Restore the stub
-//         UserModel.find.restore();
-//     });
-// });
+        // Restore the stub
+        UserModel.find.restore();
+    });
+});
--- tests/updateUser.test.js
+++ tests/updateUser.test.js
@@ -1,75 +1,75 @@
-// const request = require('supertest');
-// const app = require('../app');
-// const UserModel = require('../models/Users');
+const request = require('supertest');
+const app = require('../app');
+const UserModel = require('../models/Users');
 
-// describe('PUT /updateUser/:id', () => {
-//     let userId;
+describe('PUT /updateUser/:id', () => {
+    let userId;
 
-//     // Tạo một user giả định để kiểm tra
-//     beforeEach(async () => {
-//         const user = new UserModel({
-//             name: 'Initial User',
-//             gender: 'Male',
-//             school: 'Initial School'
-//         });
-//         const savedUser = await user.save();
-//         userId = savedUser._id;
-//     });
+    // Tạo một user giả định để kiểm tra
+    beforeEach(async () => {
+        const user = new UserModel({
+            name: 'Initial User',
+            gender: 'Male',
+            school: 'Initial School'
+        });
+        const savedUser = await user.save();
+        userId = savedUser._id;
+    });
 
-//     it('should update an existing user', async () => {
-//         const updatedData = {
-//             name: 'Updated User',
-//             gender: 'Female',
-//             school: 'Updated School'
-//         };
+    it('should update an existing user', async () => {
+        const updatedData = {
+            name: 'Updated User',
+            gender: 'Female',
+            school: 'Updated School'
+        };
 
-//         const res = await request(app)
-//             .put(`/updateUser/${userId}`)
-//             .send(updatedData);
+        const res = await request(app)
+            .put(`/updateUser/${userId}`)
+            .send(updatedData);
 
-//         expect(res.statusCode).toEqual(200);
-//         expect(res.body).toHaveProperty('_id');
-//         expect(res.body.name).toEqual(updatedData.name);
-//         expect(res.body.gender).toEqual(updatedData.gender);
-//         expect(res.body.school).toEqual(updatedData.school);
-//     });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('_id');
+        expect(res.body.name).toEqual(updatedData.name);
+        expect(res.body.gender).toEqual(updatedData.gender);
+        expect(res.body.school).toEqual(updatedData.school);
+    });
 
-//     it('should return 404 if the user does not exist', async () => {
-//         const nonExistentUserId = '60c72b2f9b1d8e4d88f08f16'; // Một ID không tồn tại trong cơ sở dữ liệu
+    it('should return 404 if the user does not exist', async () => {
+        const nonExistentUserId = '60c72b2f9b1d8e4d88f08f16'; // Một ID không tồn tại trong cơ sở dữ liệu
 
-//         const res = await request(app)
-//             .put(`/updateUser/${nonExistentUserId}`)
-//             .send({
-//                 name: 'Non-Existent User',
-//                 gender: 'Female',
-//                 school: 'Non-Existent School'
-//             });
+        const res = await request(app)
+            .put(`/updateUser/${nonExistentUserId}`)
+            .send({
+                name: 'Non-Existent User',
+                gender: 'Female',
+                school: 'Non-Existent School'
+            });
 
-//         expect(res.statusCode).toEqual(404);
-//         expect(res.body).toHaveProperty('error', 'User not found');
-//     });
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toHaveProperty('error', 'User not found');
+    });
 
-//     it('should return 400 if there is a server error', async () => {
-//         // Mock function của findByIdAndUpdate để ném một lỗi bất kỳ
-//         UserModel.findByIdAndUpdate = jest.fn().mockImplementationOnce(() => { throw new Error('Server error'); });
+    it('should return 400 if there is a server error', async () => {
+        // Mock function của findByIdAndUpdate để ném một lỗi bất kỳ
+        UserModel.findByIdAndUpdate = jest.fn().mockImplementationOnce(() => { throw new Error('Server error'); });
 
-//         const res = await request(app)
-//             .put(`/updateUser/${userId}`)
-//             .send({
-//                 name: 'Updated User',
-//                 gender: 'Female',
-//                 school: 'Updated School'
-//             });
+        const res = await request(app)
+            .put(`/updateUser/${userId}`)
+            .send({
+                name: 'Updated User',
+                gender: 'Female',
+                school: 'Updated School'
+            });
 
-//         expect(res.statusCode).toEqual(400);
-//         expect(res.body).toHaveProperty('error', 'Server error');
-//     });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('error', 'Server error');
+    });
 
-//     // Dọn dẹp sau mỗi test case
-//     afterEach(async () => {
-//         // Xóa user được tạo ra trong quá trình test
-//         if (userId) {
-//             await UserModel.findByIdAndDelete(userId);
-//         }
-//     });
-// });
+    // Dọn dẹp sau mỗi test case
+    afterEach(async () => {
+        // Xóa user được tạo ra trong quá trình test
+        if (userId) {
+            await UserModel.findByIdAndDelete(userId);
+        }
+    });
+});
