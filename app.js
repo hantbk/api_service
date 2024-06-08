a50741fbcb35f69bddf5a40307d9eae757e123d3
Ha Nguyen <captainnemot1k60@gmail.com>
2024-06-09 01:40:35 +0700

    chore: Update MongoDB connection settings to use environment variables

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+const mongoURI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}` || 'mongodb://localhost:27017/crud';
 
 mongoose.connect(mongoURI).then(() => {
     console.log("Connected to MongoDB");
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
